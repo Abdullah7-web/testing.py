b2dca3d09d2c58c6e063b3e2548c9e3b7568ecb4
Abdullah Farooq <147173493+Abdullah7-web@users.noreply.github.com>
2023-10-18 19:25:09 +0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+
+df = sns.load_dataset("Titanic")
+
+df.info()
+
+st.title("TESTING")
+st.write("This app is solely based on testing, I'm facing an issue with deployment, Creating other app to re-check the concern.")
+
+
+st.bar_chart(df, x="embark_town", y="alive")
+
+df.info()
+
+fig = px.treemap(df, path=["who", "survived"])
+st.plotly_chart(fig)
